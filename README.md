6a45badbd1db9338c8df91285e59cc8a845cd462
abey onalaja <abey.onalaja@gmail.com>
2024-07-28 11:25:04 +0100

    Add README, add example test project

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-use-please-stay
+
+A React hook that animates the document title and
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,9 @@
 import React from "react";
+import { usePleaseStay } from "./hooks/usePleaseStay";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  usePleaseStay();
+  return <> </>;
 }
 
 export default App;
